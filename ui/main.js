61f881c4f43416b8cd2a6417cefdf140479e5c92
masthanp <pmahesh9491@gmail.com>
2017-08-03 21:45:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML = "new value ";
 
@@ -12,4 +12,16 @@ function moveRight(){
 
 img.onclick = function(){
     var interval = setInterval(moveRight,50);
+};*/
+var counter = 0;
+var button =document.getElementById("counter");
+button.onclick=function(){
+  //make arequest to the end point  
+  
+  //capture the response and store it in a variable
+  
+  //render the response in the current span
+  counter =counter + 1 ;
+  var span = document.getElementById("count");
+  span.innerHTML = counter.toString();
 };
