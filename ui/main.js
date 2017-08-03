9700ec23b5af833f835b5df6760290ebabf5d641
masthanp <pmahesh9491@gmail.com>
2017-08-03 23:38:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,7 @@ img.onclick = function(){
 
 //submit name
 
-var nameInput=document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 
 submit.onclick=function(){
@@ -48,6 +47,8 @@ submit.onclick=function(){
       //not done yet
     };
     
+    var nameInput=document.getElementById("name");
+    var name = nameInput.value;
  //make the request
  request.open('GET','http://pmahesh9491.imad.hasura-app.io/submit-name?name='+name, true);
  request.send(null);
