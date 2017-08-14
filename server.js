4a8c35308b4ecd0f26be9471890c371d065564b7
masthanp <pmahesh9491@gmail.com>
2017-08-14 22:14:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/create-user',function(req,res){
     //username,password
     //Json
     var username ="mahesh";
-    var password = "password";
+    var password ="password";
     var salt=crypto.randomBytes(128).toString('hex'); 
     var dbString=hash(password,salt) ;
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
@@ -102,12 +102,13 @@ app.get('/login',function(req,res){
             }else{
                 //match the passwod
                 var dbString=result.rows[0].password;
+                var salt =dbString.split('$')[2];
                 var hashedPassword=hash(password,salt);//creating the hash based on the password submitted and orginal salt
-               /* if(hashedPassword === dbString){
+                if(hashedPassword === dbString){
                     res.send('credentials correct');
                 }else{
                     res.status(403).send('username/password is invalid')
-                }*/
+                }
                 res.send(hashedPassword);
                 
             }
