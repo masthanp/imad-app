be8dc4c8adfef049a461bc75af0088a1b0f93ce8
masthanp <pmahesh9491@gmail.com>
2017-08-14 21:46:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,7 @@ app.get('/login',function(req,res){
             }else{
                 //match the passwod
                 var dbString=result.rows[0].password;
+                  var hashedPassword=hash(password,salt);//creating the hash based on the password submitted and orginal salt
                /* dbString.split('$')[2];
                 var hashedPassword=hash(password,salt);//creating the hash based on the password submitted and orginal salt
                 if(hashedPassword === dbString){
@@ -109,7 +110,7 @@ app.get('/login',function(req,res){
                 }else{
                     res.status(403).send('username/password is invalid')
                 }*/
-                res.send(dbString);
+                res.send(hashedPassword);
             }
         }
     });
