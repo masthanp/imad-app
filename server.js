5beb659897b92c78d025967f87d583039f05479d
masthanp <pmahesh9491@gmail.com>
2017-08-15 12:02:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/check-login',function(req,res){
 });
 app.get('/logout',function(req,res){
     delete req.session.auth;
-    res.send('You are logged');
+    res.send('You are logged out');
 });
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
