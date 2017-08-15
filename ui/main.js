24cc9fe314b1f28d4966afdc31c1be13cd25888a
masthanp <pmahesh9491@gmail.com>
2017-08-15 11:14:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,107 +1,28 @@
-/*console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHTML = "new value ";
-*/
-
-//move the image
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft +"px";
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-};
-//submit name
-
-
+//submit username and password
 var submit = document.getElementById("submit_btn");
-
 submit.onclick=function(){
-    
   //create request object
     var request = new XMLHttpRequest();
-    
     request.onreadystatechange = function(){
       if(request.readyState === XMLHttpRequest.DONE)  {
           //take some action
           if(request.status === 200){
-              /*var counter =request.responseText;
-              var span = document.getElementById("count");
-              span.innerHTML = counter.toString();
-              */
-              //capture the list of name and store it as a list
-              var names = request.responseText;
-              names = JSON.parse(names);
-               
-              var list = "";
-              for(var i=0 ; i<names.length;i++){
-                  list += "<li>"+names[i] +"</li>";
-              }
-              var ul = document.getElementById("nameList");
-              ul.innerHTML =list;
+            alert("logged in successfully");
+          }else if(request.status === 403){
+              alert("username/password is incorrect");
+          }else if(request.status === 500){
+              alert("something went wrong on the server")
           }
       }
-      
       //not done yet
     };
     
-    var nameInput=document.getElementById("name");
-    var name = nameInput.value;
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
  //make the request
- request.open('GET','http://pmahesh9491.imad.hasura-app.io/submit-name?name='+name, true);
- request.send(null);
-};
-
-var counter = document.getElementById("counter");
-
-counter.onclick=function(){
-    
-  //create request object
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE)  {
-          //take some action
-          if(request.status === 200){
-              var counter =request.responseText;
-              var span = document.getElementById("count");
-              span.innerHTML = counter.toString();
-          }
-      }
-      
-      //not done yet
-    };
-    
- //make the request
- request.open('GET','http://pmahesh9491.imad.hasura-app.io/counter', true);
- request.send(null);
-};
-
-
-var comment = document.getElementById("comment");
-
-comment.onclick=function(){
-    
-  //create request object
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE)  {
-          //take some action
-          if(request.status === 200){
-              var comment =request.responseText;
-              var para = document.getElementById("comments");
-             para.innerHTML = comment;
-          }
-      }
-      
-      //not done yet
-    };
-    
- //make the request
- request.open('GET','http://pmahesh9491.imad.hasura-app.io/comments', true);
- request.send(null);
+ request.open('POST','http://pmahesh9491.imad.hasura-app.io/submit-name?name='+name, true);
+ request.setRequestHeader('Content-Type','application/json');
+ request.send(JSON.stringify({username: username, password: password}));
 };
